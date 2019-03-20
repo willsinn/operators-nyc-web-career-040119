3d6d439376c6366656fc725c52c53e07f4d13d39
w.h.sinn <w.h.sinn@gmail.com>
2019-03-20 20:49:13 +0000

    Automatically backed up by Learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,8 @@ require_relative '../lib/operations.rb'
 require_relative './operations_spec.rb'
 
 RSpec.configure do |config|
-  config.run
+    config.run_all_when_everything_filtered = true
+
     config.filter_run :focus
 
    config.order = 'default'
