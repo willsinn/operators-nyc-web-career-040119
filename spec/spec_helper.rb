8c8c7533891cccd7303bd3265ad7d902064f10b0
w.h.sinn <w.h.sinn@gmail.com>
2019-03-20 20:51:29 +0000

    Automatically backed up by Learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,9 +4,6 @@ require_relative './operations_spec.rb'
 
 RSpec.configure do |config|
     config.run_all_when_everything_filtered = true
-
     config.filter_run :focus
-
    config.order = 'default'
-
 end
