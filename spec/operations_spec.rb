4cc43ac27ca27b13c042b092fa4af11611e29faf
w.h.sinn <w.h.sinn@gmail.com>
2019-03-20 20:19:29 +0000

    Automatically backed up by Learn

--- spec/operations_spec.rb
+++ spec/operations_spec.rb
@@ -1,3 +1,5 @@
+require_relative '../lib/operations.rb'
+require_relative '../lib/operations.rb'
 
 describe "Operations" do
   let(:file_contents) { File.read(File.join(File.dirname(__FILE__), '..', 'lib/operations.rb')) }
