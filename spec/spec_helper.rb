86c02a7861cebddd8ffee0f0a05b998152e018f0
w.h.sinn <w.h.sinn@gmail.com>
2019-03-20 20:56:07 +0000

    Automatically backed up by Learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,7 @@
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
-require_relative './operations_spec.rb'
 require_relative '../lib/operations.rb'
 
 
 RSpec.configure do |config|
-    config.run_all_when_everything_filtered = true
-    config.filter_run :focus
-   config.order = 'default'
+  #config here
 end
